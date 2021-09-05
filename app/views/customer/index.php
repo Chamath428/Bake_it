a611f88eebf704f165b773dce3e724d301d8a6bb
Chamath Chinthana <62209498+Chamath428@users.noreply.github.com>
2021-09-05 21:52:29 +0530

    after footer

--- app/views/customer/index.php
+++ app/views/customer/index.php
@@ -53,7 +53,7 @@
 			<!-- Navigation Bar Ends -->
 		</div>
 			<div class="hero-bottom">
-					<h1 class="header1">Best Taste Scince 1997</h1>
+					<h1 class="header1">Best Taste Since 1997</h1>
 					<button class="dismenu-btn"><a href="#menu">Discover Menu</a></button>
 			</div>
 		</div>
@@ -70,28 +70,8 @@
 				</div>
 				<div class="menu-box grid-item-2">
 					<a href="#">
-						<img src="../../../public/images/donuts.jpg">
-						<div class="h3">DONUT</div>
-					</a>
-				</div>
-
-				<div class="menu-box grid-item-3">
-					<a href="#">
-						<img src="../../../public/images/muffins.jpg">
-						<div class="h3">MUFFIN</div>
-					</a>
-				</div>
-				<div class="menu-box grid-item-4">
-					<a href="#">
 						<img src="../../../public/images/pastry.jpg">
 						<div class="h3">PASTRY</div>
-					</a>
-				</div>
-
-				<div class="menu-box grid-item-5">
-					<a href="#">
-						<img src="../../../public/images/burger.jpg">
-						<div class="h3">BURGER</div>	
 					</a>
 				</div>
 				<div class="menu-box grid-item-6">
@@ -100,7 +80,73 @@
 						<div class="h3">CAKE</div>
 					</a>
 				</div>
+				<div class="menu-box grid-item-5">
+					<a href="#">
+						<img src="../../../public/images/burger.jpg">
+						<div class="h3">BURGER</div>	
+					</a>
+				</div>
+				<div class="menu-box grid-item-9">
+					<a href="#">
+						<img src="../../../public/images/snacks.jpg">
+						<div class="h3">SNACKS</div>
+					</a>
+				</div>
+				<div class="menu-box grid-item-3">
+					<a href="#">
+						<img src="../../../public/images/donuts.jpg">
+						<div class="h3">DONUT</div>
+					</a>
+				</div>
+
+				<div class="menu-box grid-item-4">
+					<a href="#">
+						<img src="../../../public/images/muffins.jpg">
+						<div class="h3">MUFFIN</div>
+					</a>
+				</div>
+				<div class="menu-box grid-item-7">
+					<a href="#">
+						<img src="../../../public/images/sweets.jpg">
+						<div class="h3">SWEETS</div>
+					</a>
+				</div>
+				<div class="menu-box grid-item-8">
+					<a href="#">
+						<img src="../../../public/images/baverages.jpg">
+						<div class="h3">BAVERAGES</div>
+					</a>
+				</div>
 			</div>
 		</section>
+
+		<footer class="footer">
+			<div class="footer-container">
+				<div class="footer-row">
+					<div class="footer-column fot-first">
+						<ul>
+							<li><a href="#">Reviews</a></li>
+							<li><a href="#">Delivery Areas</a></li>
+							<li><a href="#">Privercy Policy</a></li>
+							<li><a href="#">Terms and Conditions</a></li>
+						</ul>
+					</div>
+					<div class="footer-column contract-us">
+						<ul>
+							<li><i class="fas fa-phone-square-alt"></i>+94118659322 (Main Bakery)</li>
+							<li><i class="fas fa-at"></i> writetobakeit@gmail.com</li>	
+							<li><i class="fas fa-map-marker-alt"></i> NO.38/5 Baththramulla</li>
+						</ul>
+					</div>
+					<div class="footer-column social-media">
+						<ul>
+							<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram"></i></li>	
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+
 	</body>
 
