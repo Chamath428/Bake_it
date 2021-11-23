d0dea84df844ac48f2f7e2b871dfba79e16918d1
Chamath Chinthana <62209498+Chamath428@users.noreply.github.com>
2021-11-23 23:24:19 +0530

    checkoutHalfWay

---
+++ application/models/checkoutModel.php
@@ -0,0 +1,103 @@
+<?php 
+
+	/**
+	 * 
+	 */
+	class checkoutModel extends database
+	{
+		
+		function __construct()
+		{
+			$this->db=$this->dbcon();
+		}
+
+		public function createCustomer($customerData){
+			 $customerData['first_name']=$this->db->real_escape_string($customerData['first_name']);
+			 $customerData['last_name']=$this->db->real_escape_string($customerData['last_name']);
+			 $customerData['phone_number']=$this->db->real_escape_string($customerData['phone_number']);
+			 $customerData['address1']=$this->db->real_escape_string($customerData['address1']);
+			 $customerData['address2']=$this->db->real_escape_string($customerData['address2']);
+			 $customerData['address3']=$this->db->real_escape_string($customerData['address3']);
+
+			 $sql1="	INSERT INTO
+						customer(
+								 first_name,
+								 last_name,
+								 address1,
+								 address2,
+								 address3,
+								 customer_type
+								)
+						VALUES  ("
+								.'"'.$customerData['first_name'].'"' 	.","
+								.'"'.$customerData['last_name'].'"' 		.","
+								.'"'.$customerData['address1'].'"' 			.","
+								.'"'.$customerData['address2'].'"' 			.","
+								.'"'.$customerData['address3'].'"' 			.",
+								 2)";
+
+			$res1=mysqli_query($this->db,$sql1) or die('1->'.mysqli_error($this->db));
+
+			$sql2= "SELECT LAST_INSERT_ID() AS last_id";
+			$res2=mysqli_query($this->db,$sql2) or die('2->'.mysqli_error($this->db));
+			$row=mysqli_fetch_assoc($res2);
+			$last_id=$row['last_id'];
+
+			$sql3="	INSERT INTO
+						unregistered_customer(
+											contact_number,
+											customer_id)
+									VALUES("
+											.'"'.$customerData['phone_number'].'"'		.","
+												.$last_id.")";
+
+			$res3=mysqli_query($this->db,$sql3) or die('3->'.mysqli_error($this->db));
+
+			return $last_id;
+		}
+
+		public function placeOrder($orderDetails){
+			 $orderDetails['customer_id']=$this->db->real_escape_string($orderDetails['customer_id']);
+			 $orderDetails['subtotal']=$this->db->real_escape_string($orderDetails['subtotal']);
+			 $orderDetails['delivery_type']=$this->db->real_escape_string($orderDetails['delivery_type']);
+			 $orderDetails['payment_type']=$this->db->real_escape_string($orderDetails['payment_type']);
+			 $orderType=1;
+			 $orderStatus=1;
+			 if (isset($_SESSION['branch_Id'])) {
+			 	$menuId=$_SESSION['branch_Id'];
+			 }
+
+			 $sql4="INSERT INTO
+			 			order_details(
+							customer_id,
+							menu_id,
+							order_type,
+							total_amount,
+							reveiving_method,
+							payment_type,
+							order_status
+						)
+					VALUES ("
+							.'"'.$orderDetails['customer_id'].'"' 	.","
+							.'"'.$menuId.'"' 						.","
+							.'"'.$orderType.'"' 					.","
+							.'"'.$orderDetails['subtotal'].'"' 		.","
+							.'"'.$orderDetails['delivery_type'].'"' .","
+							.'"'.$orderDetails['payment_type'].'"' 	.","
+							.'"'.$orderStatus.'")';
+			$res4=mysqli_query($this->db,$sql4) or die('4->'.mysqli_error($this->db));
+
+			$sql5= "SELECT LAST_INSERT_ID() AS last_order_id";
+			$res5=mysqli_query($this->db,$sql5) or die('5->'.mysqli_error($this->db));
+			$row2=mysqli_fetch_assoc($res5);
+			$last_order_id=$row2['last_order_id'];
+
+			return $last_order_id;
+		}
+
+		public function insertOrderItems($order_id,$menu_id,$items){
+
+		}
+	}
+
+ ?>
